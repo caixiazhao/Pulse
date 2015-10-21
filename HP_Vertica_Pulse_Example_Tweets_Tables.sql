d7525d4fe24dd0843402f4d691c80f950b3838f4
bjnoe <benedict.noero@hp.com>
2015-10-21 18:09:43 -0400

    putting back some changes

--- HP_Vertica_Pulse_Example_Tweets_Tables.sql
+++ HP_Vertica_Pulse_Example_Tweets_Tables.sql
@@ -30,16 +30,3 @@ CREATE TABLE tweets(id int, created_at TIMESTAMPTZ, "user.name" VARCHAR(144), "u
 
 -- Create a tweet sentiment table to hold the sentiment scores for the analyzed tweets
 CREATE TABLE tweet_sentiment AS(SELECT id, "user.screen_name", SentimentAnalysis(text USING PARAMETERS filterlinks = true, filterusermentions = true) OVER(PARTITION BY id, "user.screen_name", text) FROM tweets WHERE lang = 'en' ORDER BY attribute);
-
--- Create a DML query to take the analyzed data and insert it into the tweet sentiment table. This query should be saved as a .sql file somewhere on your local file system
-INSERT INTO tweet_sentiment(SELECT id, "user.screen_name", SentimentAnalysis(text USING PARAMETERS filterlinks = true, filterusermentions = true) OVER(PARTITION BY id, "user.screen_name", text) FROM tweets WHERE lang = 'en' AND      tweets.created_at > (SELECT dt FROM dt_start) AND tweets.created_at <= (SELECT dt FROM dt_end) ORDER BY attribute);
-
--- The dt_start and dt_end tables are used to keep track of which tweets have been analyzed and which are still in the "queue"
-CREATE TABLE dt_start AS (SELECT max(created_at) dt FROM tweets);
-DROP TABLE IF EXISTS dt_end;
-CREATE TABLE dt_end AS(SELECT max(created_at) dt FROM tweets);
-DROP TABLE IF EXISTS dt_start;
-CREATE TABLE dt_start AS (SELECT dt FROM dt_end);
-
--- Release the JVM memory pool
-SELECT release_jvm_memory();

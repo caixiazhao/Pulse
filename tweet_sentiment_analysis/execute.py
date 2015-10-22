b3678745026ce276677a2d5d8be49268d5b3fa55
bjnoe <benedict.noero@hpe.com>
2015-10-22 11:15:15 -0400

    Update execute.py

--- tweet_sentiment_analysis/execute.py
+++ tweet_sentiment_analysis/execute.py
@@ -1,5 +1,9 @@
 # First install python twisted before running this script: 
 # (Red Hat) sudo yum install python-twisted / (Ubuntu) sudo apt-get install python-twisted
+# To run the SentimentAnalysis() function in HP Vertica and insert the analyzed data into the tweet_sentiment table,
+# you need some kind of scheduler. One option is to use cron, however cron can only run a script once every minute at its
+# max speed. To run SentimentAnalysis() and push the analyzed data into the tweet_sentiment table in an interval shorter than
+# once a minute, use this script. This script runs the tweet_update.sh script every second.
 #!/usr/bin/python
 
 from twisted.internet import task
